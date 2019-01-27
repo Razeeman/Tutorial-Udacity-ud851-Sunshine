e1ca8c00073630aa0f73e3a4aee007cf382d9ce1
Razeeman <freeraz@mail.ru>
2019-01-27 10:38:21 +0300

    Populated weather ScrollView with fake data.

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,12 +17,34 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mWeatherDisplayTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
+
+        mWeatherDisplayTextView = findViewById(R.id.tv_weather_data);
+
+        String[] fakeWeatherData = {
+                "Today, Cold, -11°C",
+                "Tomorrow, Cold, -15°C",
+                "Tuesday, Cold, -7°C",
+                "Wednesday, Cold, -12°C",
+                "Thursday, Freezing, -17°C",
+                "Friday, Freezing, -21°C",
+                "Saturday, Stay at Home, -25°C",
+                "Feb 3, Freezing, -14°C",
+                "Feb 4, Cold, -9°C",
+                "Feb 5, Cold, -5°C",
+        };
+
+        for (String weatherDay: fakeWeatherData) {
+            mWeatherDisplayTextView.append(weatherDay + "\n\n\n");
+        }
     }
 }
