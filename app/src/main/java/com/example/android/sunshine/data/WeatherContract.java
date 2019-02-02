1fa7fe4c155c07515db14755fb8110bbd38da735
Razeeman <freeraz@mail.ru>
2019-02-02 16:18:48 +0300

    Added weather content provider.

--- app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
+++ app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
@@ -1,34 +1,44 @@
 package com.example.android.sunshine.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class WeatherContract {
 
-    /** Defines the structure of weather table */
+    // Name of the content provider.
+    public static final String CONTENT_AUTHORITY = "com.example.android.sunshine";
+
+    // Base uri for all contacts with content provider.
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    // Possible paths from base uri.
+    public static final String PATH_WEATHER = "weather";
+
+    // Defines the structure of weather table
     public static final class WeatherEntry implements BaseColumns {
-        /** Name of the table */
+        // Name of the table.
         public static final String TABLE_NAME = "weather";
 
-        /** UTC date */
+        // UTC date.
         public static final String COLUMN_DATE = "date";
 
-        /** Weather Id */
+        // Weather Id.
         public static final String COLUMN_WEATHER_ID = "weather_id";
 
-        /** Min and max temperature for the day in celsius degrees, stored as a float */
+        // Min and max temperature for the day in celsius degrees, stored as a float.
         public static final String COLUMN_MIN_TEMP = "min";
         public static final String COLUMN_MAX_TEMP = "max";
 
-        /** Humidity for the day in percentage, stored as a float */
+        // Humidity for the day in percentage, stored as a float.
         public static final String COLUMN_HUMIDITY = "humidity";
 
-        /** Pressure for the day in percentage, stored as a float */
+        // Pressure for the day in percentage, stored as a float.
         public static final String COLUMN_PRESSURE = "pressure";
 
-        /** Wind speed for the day, stored as a float */
+        // Wind speed for the day, stored as a float.
         public static final String COLUMN_WIND_SPEED = "wind";
 
-        /** Meteorological degrees (0 is north, 180 is south), stored as a float */
+        // Meteorological degrees (0 is north, 180 is south), stored as a float.
         public static final String COLUMN_DEGREES = "degrees";
     }
 }
---
+++ app/src/main/java/com/example/android/sunshine/data/WeatherProvider.java
@@ -0,0 +1,187 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.sunshine.data;
+
+import android.annotation.TargetApi;
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+/**
+ * This class serves as the ContentProvider for all of Sunshine's data. This class allows us to
+ * bulkInsert data, query data, and delete data.
+ */
+public class WeatherProvider extends ContentProvider {
+
+    public static final int CODE_WEATHER = 100;
+    public static final int CODE_WEATHER_WITH_DATE = 101;
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    WeatherDbHelper mOpenHelper;
+
+    /**
+     * Creates the UriMatcher that will match each URI to the CODE_WEATHER and
+     * CODE_WEATHER_WITH_DATE constants defined above.
+     *
+     * @return A UriMatcher that correctly matches the constants for CODE_WEATHER and CODE_WEATHER_WITH_DATE
+     */
+    public static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = WeatherContract.CONTENT_AUTHORITY;
+
+        // This URI is content://com.example.android.sunshine/weather/
+        matcher.addURI(authority, WeatherContract.PATH_WEATHER, CODE_WEATHER);
+
+        // This URI looks like content://com.example.android.sunshine/weather/1472214172
+        matcher.addURI(authority, WeatherContract.PATH_WEATHER + "/#", CODE_WEATHER_WITH_DATE);
+
+        return matcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = new WeatherDbHelper(getContext());
+        return true;
+    }
+
+    /**
+     * Handles requests to insert a set of new rows.
+     *
+     * @param uri    The content:// URI of the insertion request.
+     * @param values An array of sets of column_name/value pairs to add to the database.
+     *               This must not be null.
+     *
+     * @return       The number of values that were inserted.
+     */
+    @Override
+    public int bulkInsert(@NonNull Uri uri, @NonNull ContentValues[] values) {
+        throw new RuntimeException("Student, you need to implement the bulkInsert method!");
+    }
+
+    /**
+     * Handles query requests from clients.
+     *
+     * @param uri           The URI to query
+     * @param projection    The list of columns to put into the cursor. If null, all columns are
+     *                      included.
+     * @param selection     A selection criteria to apply when filtering rows. If null, then all
+     *                      rows are included.
+     * @param selectionArgs You may include ?s in selection, which will be replaced by
+     *                      the values from selectionArgs, in order that they appear in the
+     *                      selection.
+     * @param sortOrder     How the rows in the cursor should be sorted.
+     * @return              A Cursor containing the results of the query.
+     */
+    @Override
+    public Cursor query(@NonNull Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+
+        Cursor cursor;
+
+        switch (sUriMatcher.match(uri)) {
+            case CODE_WEATHER_WITH_DATE: {
+                String UTCdate = uri.getLastPathSegment();
+                String[] selectionArguments = new String[] {UTCdate};
+
+                cursor = mOpenHelper.getReadableDatabase().query(
+                        WeatherContract.WeatherEntry.TABLE_NAME,
+                        projection,
+                        WeatherContract.WeatherEntry.COLUMN_DATE + " = ? ",
+                        selectionArguments,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            }
+
+            case CODE_WEATHER: {
+                cursor = mOpenHelper.getReadableDatabase().query(
+                        WeatherContract.WeatherEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            }
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
+    }
+
+    /**
+     * Deletes data at a given URI with optional arguments for more fine tuned deletions.
+     *
+     * @param uri           The full URI to query
+     * @param selection     An optional restriction to apply to rows when deleting.
+     * @param selectionArgs Used in conjunction with the selection statement
+     * @return              The number of rows deleted
+     */
+    @Override
+    public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
+        throw new RuntimeException("Student, you need to implement the delete method!");
+    }
+
+    /**
+     * Not used.
+     *
+     * @param uri The URI to query.
+     * @return    Normally a MIME type string, or null if there is no type.
+     */
+    @Override
+    public String getType(@NonNull Uri uri) {
+        throw new RuntimeException("We are not implementing getType in Sunshine.");
+    }
+
+    /**
+     * Not used.
+     *
+     * @param uri    The URI of the insertion request. This must not be null.
+     * @param values A set of column_name/value pairs to add to the database.
+     *               This must not be null
+     * @return       Normally the URI for the newly inserted item.
+     */
+    @Override
+    public Uri insert(@NonNull Uri uri, ContentValues values) {
+        throw new RuntimeException(
+                "We are not implementing insert in Sunshine. Use bulkInsert instead");
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        throw new RuntimeException("We are not implementing update in Sunshine");
+    }
+
+    /**
+     * Assist the testing.
+     */
+    @Override
+    @TargetApi(11)
+    public void shutdown() {
+        mOpenHelper.close();
+        super.shutdown();
+    }
+}
